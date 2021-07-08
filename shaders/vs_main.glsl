3fc79eb57948f4f0453dec4d19ce29ced81a6369
IngSorre97 <10566104@mail.polimi.it>
2021-07-08 20:33:57 +0200

    Fix bug objects not spawning

--- shaders/vs_main.glsl
+++ shaders/vs_main.glsl
@@ -4,9 +4,9 @@ in vec3 a_position;
 in vec3 a_normal;
 in vec2 a_uv;
 
-uniform mat4 viewWorldMatrix;
-uniform mat4 projectionMatrix;
-uniform mat4 normalMatrix;
+uniform mat4 u_viewWorldMatrix;
+uniform mat4 u_projectionMatrix;
+uniform mat4 u_normalMatrix;
 
 out vec3 v_position;
 out vec3 v_normal;
@@ -14,10 +14,10 @@ out vec2 v_uv;
 
 void main() {
 
-    v_normal = mat3(normalMatrix) * a_normal;
-	v_position = (viewWorldMatrix * vec4(a_position, 1.0)).xyz;
+    v_normal = mat3(u_normalMatrix) * a_normal;
+	v_position = (u_viewWorldMatrix * vec4(a_position, 1.0)).xyz;
 	
 	v_uv = vec2(a_uv.x, 1.0 - a_uv.y);
 	
-	gl_Position = projectionMatrix * vec4(a_position, 1.0);
+	gl_Position = u_projectionMatrix * vec4(a_position, 1.0);
 }
