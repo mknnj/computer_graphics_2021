ea0e7ce3304aab5439607fd9ce1201daa51bdefc
mknnj <miki199714@gmail.com>
2021-07-22 16:09:17 +0200

    fix skybox shader

--- shaders/vs_skybox.glsl
+++ shaders/vs_skybox.glsl
@@ -2,8 +2,10 @@
 
 in vec4 a_position;
 out vec4 v_position;
+
+uniform mat4 u_viewDirectionProjectionInverse;
 void main() {
-  v_position = a_position;
+  v_position = u_viewDirectionProjectionInverse * a_position;
   gl_Position = a_position;
   gl_Position.z = 1.0;
 }
